e872efd713782438e9ba9d29fa8a2ee91f762856
Rakhsrb <suhrobrahmatullayev973132@gmail.com>
2025-03-24 23:08:17 +0500

    In the code was the problem with data which comming from api that could not be there therefore i fixed that bag and it will never look again

--- src/modules/ProjectsModule.jsx
+++ src/modules/ProjectsModule.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { ProjectCard } from "./partials/ProjectCard";
 import { Pending } from "../components/Pending";
@@ -44,7 +44,7 @@ export const ProjectsModule = () => {
       <div data-aos="fade-up" className="container">
         <h1
           className={`text-4xl md:text-5xl font-bold mb-20 text-red-800 text-center ${
-            location.pathname == "/" ? "" : "hidden"
+            location.pathname === "/" ? "" : "hidden"
           }`}
         >
           {t("headings.projects.mainTitle")}
@@ -71,27 +71,43 @@ export const ProjectsModule = () => {
         </div>
         {isLoading ? (
           <Pending />
-        ) : data.data.length > 0 ? (
-          <div className="pt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
-            {data.data.map((item, index) => (
-              <ProjectCard data-aos="zoom-in" key={index} item={item} />
-            ))}
+        ) : error ? (
+          <div className="mt-3 rounded-l-xl h-[30vh] flex items-center justify-center bg-slate-100">
+            <h1 className="text-red-800 font-bold text-3xl">
+              Error loading data
+            </h1>
           </div>
+        ) : data && data.data ? (
+          data.data.length > 0 ? (
+            <div className="pt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+              {data.data.map((item, index) => (
+                <ProjectCard data-aos="zoom-in" key={index} item={item} />
+              ))}
+            </div>
+          ) : (
+            <div className="mt-3 rounded-l-xl h-[30vh] flex items-center justify-center bg-slate-100">
+              <h1 className="text-red-800 font-bold text-3xl">NO DATA</h1>
+            </div>
+          )
         ) : (
           <div className="mt-3 rounded-l-xl h-[30vh] flex items-center justify-center bg-slate-100">
             <h1 className="text-red-800 font-bold text-3xl">NO DATA</h1>
           </div>
         )}
-        {data?.data?.length > 0 && data?.total > data?.data?.length && (
-          <div className="flex justify-center mt-5">
-            <button
-              onClick={handleLoadMore}
-              className="bg-[#5D75A5] text-white font-semibold py-2 px-6 rounded-lg hover:bg-[#4A5F88] transition-all"
-            >
-              Ko'proq ko'rish
-            </button>
-          </div>
-        )}
+        {data &&
+          data.data &&
+          data.total &&
+          data.data.length > 0 &&
+          data.total > data.data.length && (
+            <div className="flex justify-center mt-5">
+              <button
+                onClick={handleLoadMore}
+                className="bg-[#5D75A5] text-white font-semibold py-2 px-6 rounded-lg hover:bg-[#4A5F88] transition-all"
+              >
+                Ko'proq ko'rish
+              </button>
+            </div>
+          )}
       </div>
     </section>
   );
