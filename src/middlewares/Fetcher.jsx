a1f897b79d74fca8e17b432f763b2fd3c52e9d56
Rahmatullayev Suhrob <108258306+Rakhsrb@users.noreply.github.com>
2025-03-26 12:45:46 +0500

    Update Fetcher.jsx

--- src/middlewares/Fetcher.jsx
+++ src/middlewares/Fetcher.jsx
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:5000/api";
+// export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL = "https://samobitzer-server.onrender.com/api";
 
 export const fetcher = (url) =>
   fetch(`${BASE_URL}${url}`).then((res) => res.json());
