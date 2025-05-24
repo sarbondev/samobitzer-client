29fbb6d3053c6cc723b80d19ce5df4e24be6d9df
sarbondev <suhrobrahmatullayev973132@gmail.com>
2025-05-24 12:33:11 +0500

    deleted partners

--- src/modules/PartnersModule.jsx
+++ src/modules/PartnersModule.jsx
@@ -1,20 +1,7 @@
-import React from "react";
 import { useTranslation } from "react-i18next";
 
 export const PartnersModule = () => {
-  const partners = [
-    "./bananza.png",
-    "./bitzer.png",
-    "./ecobozor.png",
-    "./lactel.png",
-    "./makro.png",
-    "./mumtoz.png",
-    "./nestle.png",
-    "./rashmilk.png",
-    "./rozmetov.png",
-    "./sherin.png",
-    "./uzbegimqora.png",
-  ];
+  const partners = [];
 
   const { t } = useTranslation();
 
@@ -24,15 +11,17 @@ export const PartnersModule = () => {
         {t("partners")}
       </h1>
       <div className="container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
-        {partners.map((item, index) => (
-          <img
-            data-aos="fade-up"
-            key={index}
-            src={item}
-            alt=""
-            className="object-cover p-[25px] md:p-[50px]"
-          />
-        ))}
+        {partners.length > 0
+          ? partners.map((item, index) => (
+              <img
+                data-aos="fade-up"
+                key={index}
+                src={item}
+                alt=""
+                className="object-cover p-[25px] md:p-[50px]"
+              />
+            ))
+          : "no data"}
       </div>
     </section>
   );
